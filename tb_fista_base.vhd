9882b61d36eab5b182a20c4c3accd9cfc0636c1c
mathFPGAseek <baycool0422@gmail.com>
2022-12-14 20:13:26 -0800

    Added diag for debug; Take sequence  of addrs that have been reversed and then sequenced.
    Still have mismatch to matlab

--- tb_fista_base.vhd
+++ tb_fista_base.vhd
@@ -124,6 +124,7 @@ architecture tb of tb_xfft_0 is
   type result_type is ( '0', '1');
   signal write_line_done : result_type;
   signal write_fft_1d_done : result_type;
+  signal write_fft_1d_seq_done : result_type;
 
   -----------------------------------------------------------------------
   -- DUT signals
@@ -217,6 +218,15 @@ architecture tb of tb_xfft_0 is
        32,  33,  34, 35,   36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
        64,  65, 66,  67,   68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
        96,  97, 98,  99,  100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127 );
+   signal fft_bin_seq_addr : bit_addr :=
+    (   0,   1,   2,  3,    4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
+       32,  33,  34, 35,   36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
+       64,  65, 66,  67,   68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
+       96,  97, 98,  99,  100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127,
+      128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
+      160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191,
+      192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223,
+      224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 222, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255 );
 
     -------------------------------------------------
 	-- DEBUG :Write to a file for debugging
@@ -243,6 +253,40 @@ architecture tb of tb_xfft_0 is
 	      report" Done writing to file ";	  
   	      return result;  	       
      end function  writeToFileDebugTransferLine;
+     
+    -------------------------------------------------
+	-- Write to a file the mem contents to check
+	-------------------------------------------------
+	impure function writeToFileMemSeqContents(  signal fft_mem   : in MEM_ARRAY;
+		                                       signal fft_bin_center_addr : in bit_addr) return result_type is
+	   variable result       : result_type;    
+	   variable mem_line_var : line;
+	   variable done         : integer;
+	   variable k            : integer;
+	   variable fft_spec     : MEM_ARRAY;
+	   variable data_write_var : bit_vector(67 downto 0);
+	   begin
+	   	 	for i in  0 to MAX_SAMPLES-1 loop
+	         for j in 0 to MAX_SAMPLES-1 loop
+	            k := fft_bin_center_addr(j);
+	            fft_spec(i,k) := (fft_mem(i,j));
+	         end loop;
+	      end loop;
+	     file_open(write_file,"fft_1d_mem_seq_vectors.txt",write_mode);
+	     report" File Opened for writing ";
+	          for i in  0 to MAX_SAMPLES-1 loop
+	              for j in 0 to MAX_SAMPLES-1 loop
+	                  data_write_var := to_bitvector(fft_spec(i,j));
+	                  write(mem_line_var ,data_write_var);
+	                  writeline(write_file,mem_line_var);                  
+	                  --report" Start writing to file ";
+	              end loop;
+	          end loop;
+	      done := 1;
+	      file_close(write_file);
+	      report" Done writing to file ";	  
+  	    return result;  	       
+     end function  writeToFileMemSeqContents;
    
   -------------------------------------------------
 	-- Write to a file the mem contents to check
@@ -631,8 +675,12 @@ begin
         index_start <= index_start + MAX_SAMPLES;
     end loop;
 
-    -- write to file
+    -- report write to file
     report " start writing fft 1d file";
+    
+    --write to file the reordered sequence
+    write_fft_1d_seq_done <= writeToFileMemSeqContents(fft_mem,fft_bin_seq_addr);
+    	
     --write_fft_1d_done <= writeToFileMemContents(fft_mem);
     write_fft_1d_done <= writeToFileMemContents(fft_mem,fft_bin_center_addr);
     -- completed 1-d fft
